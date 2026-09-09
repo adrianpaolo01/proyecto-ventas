682f9dc2639dbe24fe1088faeb703dd04a455782
Adrian Paolo <adrianpaolovalverde@gmail.com>
2026-09-09 16:29:41 -0400

    fix: arreglando la funcionalidad de impuestos

--- src/ventas.js
+++ src/ventas.js
@@ -1,7 +1,21 @@
 class Ventas{
     cantidadItems = 0;
     precioItem = 0;
-    
+
+    // diccionario de siglas de estados de USA con sus impuestos (10 ejemplos)
+    siglasEstados = {
+        "CA": 0.075, // 7.5%
+        "NY": 0.04, // 4%
+        "TX": 0.0625, // 6.25%
+        "FL": 0.06, // 6%
+        "IL": 0.0625,
+        "OH": 0.0575,
+        "GA": 0.04,
+        "NC": 0.0475,
+        "SC": 0.06
+    }
+
+
     obtenerCantidadItems(cantItems){
         this.cantidadItems = cantItems;
         return this.cantidadItems;
@@ -11,6 +25,20 @@ class Ventas{
         this.precioItem = precioItem
         return this.precioItem
     }
+
+    obtenerPrecioNeto(cantItems, precioItem){
+        let precioNeto = cantItems * precioItem
+        return precioNeto
+    }
+
+    aplicarImpuesto(sigla){
+        let impuesto = this.siglasEstados[sigla]
+        let precioNeto = this.obtenerPrecioNeto(this.cantidadItems, this.precioItem)
+        let precioFinal = precioNeto + (precioNeto * impuesto)
+        return precioFinal
+    }
+
+    
 
 
 }
--- src/ventas.spec.js
+++ src/ventas.spec.js
@@ -2,7 +2,7 @@ import Ventas from "./ventas"
 
 describe( "Ventas",() =>{
     
-   it("al ingresar el numero de ventas, debe mostrar el numero de ventas", () => {
+   it("al ingresar el cantidad de items, debe mostrar el cantidad de items", () => {
     let ventas = new Ventas()
         expect(ventas.obtenerCantidadItems(5)).toEqual(5); 
   });
@@ -12,5 +12,25 @@ describe( "Ventas",() =>{
         expect(ventas.obtenerPrecioItem(10)).toEqual(10); 
   });
   
+  it("al ingresar el precio y cantidad de items, debe mostrar el precio neto", () => {
+    let ventas = new Ventas()
+        expect(ventas.obtenerPrecioNeto(5,2)).toEqual(10); 
+  });
+
+  it("al ingresar el precio y cantidad de items, debe mostrar el precio neto", () => {
+    let ventas = new Ventas()
+        expect(ventas.obtenerPrecioNeto(5,2)).toEqual(10); 
+  });
+
+  it("al ingresar una sigla de estado, debe mostrar el precio final con impuesto", () => {
+    let ventas = new Ventas()
+    ventas.obtenerCantidadItems(3)
+    ventas.obtenerPrecioItem(20)
+    ventas.obtenerPrecioNeto(3,20)
+    
+    expect(ventas.aplicarImpuesto("CA")).toEqual(64.5); 
+  });
+
+
 });
 
